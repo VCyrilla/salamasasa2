cac03fb8b099b5af37c057bb944c9c360824c391
VCyrilla <vanessa.cyrilla@strathmore.edu>
2024-05-21 01:44:05 +0300

    small CSS change

--- codeigniter4-framework-9b2cd73/app/Views/layouts/inc/navbar.php
+++ codeigniter4-framework-9b2cd73/app/Views/layouts/inc/navbar.php
@@ -1,3 +1,4 @@
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -14,13 +15,13 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Log in
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Patient</a></li>
+            <li><a class="dropdown-item" href="#">Doctor</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Admin</a></li>
           </ul>
         </li>
         <li class="nav-item">
